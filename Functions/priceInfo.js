737e4ed63c66ee67860afb57cafe6eec617a17b0
David Song <david.jh.song@gmail.com>
2024-02-24 16:40:55 +0900

    Message indentation fix

--- Functions/priceInfo.js
+++ Functions/priceInfo.js
@@ -49,27 +49,37 @@ function generatePriceMsg(name) {
     actualPrice = voucherList[type][days].actualPrice;
     return `[인천 아이미래로]
 
-    ${name} 산모님~♡ 예약금 관련해서 안내 드립니다 :)
-    
-    서비스 기간: 출퇴근 ${weeks}주 (평일기준 ${days}일)
-    정부지원 바우처 유형: ${type}
-    
-    기본 서비스 금액은 총 ${fullPrice}원이며, 
-    정부 지원금액은 ${grant}원 입니다.
-    
-    산모님께서 부담하시는 금액은 ${actualPrice}원 입니다.
-    
-    서비스 예약을 위해 선납하실 예약금은 100,000원 입니다.
-    예약금 입금 후에 서비스 예약이 확정 됩니다.
-    
-    입금 계좌번호: 
-    ${bankAccountList[area].bankName} ${bankAccountList[area].accNum}
-    예금주: 인천 아이미래로 (김정인)
-    
-    입금시 입금자명을 꼭 기재해 주세요 :)
-    (타인 계좌에서 송금시 산모님 성함 기재 필수)
-    
-    감사합니다.`
+${name} 산모님~♡ 
+예약금 관련해서 안내 드립니다 :)
+
+서비스 기간: 
+    출퇴근 ${weeks}주 (평일기준 ${days}일)
+정부지원 바우처 유형: 
+    ${type}
+
+기본 서비스 금액은 
+총 ${fullPrice}원이며, 
+정부 지원금액은 
+${grant}원 입니다.
+
+산모님께서 부담하시는 금액은 
+${actualPrice}원 입니다.
+
+서비스 예약을 위해 
+선납하실 예약금은 
+100,000원 입니다.
+
+예약금 입금 후에 
+서비스 예약이 확정 됩니다.
+
+입금 계좌번호: 
+${bankAccountList[area].bankName} ${bankAccountList[area].accNum}
+예금주: 인천 아이미래로 (김정인)
+
+입금시 입금자명을 꼭 기재해 주세요 :)
+(타인 계좌에서 송금시 산모님 성함 기재 필수)
+
+감사합니다.`
 }
 
 export default async function priceInfo() {
