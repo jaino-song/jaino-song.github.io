051a0acdab654fb94e992500aa0bed08e1593d91
David Song <david.jh.song@gmail.com>
2025-10-07 21:41:18 -0700

    feat: gh actions workflow established

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -4,8 +4,8 @@ const Contact = () => {
   return (
     <section className="w-full flex flex-col items-center justify-center gap-10 py-30">
         <article className="w-full flex flex-col items-center justify-center gap-8">
-            <h2 className="self-stretch text-center justify-start text-white text-5xl font-bold font-['Poppins'] leading-10">Let's Work Together</h2>
-            <h4 className="self-stretch text-center justify-start text-gray-300 text-xl font-medium font-['Poppins'] leading-snug">I'm always interested in new opportunities and exciting projects.</h4>
+            <h2 className="self-stretch text-center justify-start text-white text-5xl font-bold font-['Poppins'] leading-10">Let&apos;s Work Together</h2>
+            <h4 className="self-stretch text-center justify-start text-gray-300 text-xl font-medium font-['Poppins'] leading-snug">I&apos;m always interested in new opportunities and exciting projects.</h4>
         </article>
         <article className="w-full flex items-center justify-center gap-4">
             <ContactBtn text="david.jh.song@gmail.com" img="/svg/Mail.svg" link="mailto:david.jh.song@gmail.com" />
