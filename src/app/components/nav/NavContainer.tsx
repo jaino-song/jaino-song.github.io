5d62078dc24060a6ecd884998c4ae9193fd2080f
David Song <david.jh.song@gmail.com>
2025-10-13 13:50:17 -0700

    fix: nav toast added

--- src/app/components/nav/NavContainer.tsx
+++ src/app/components/nav/NavContainer.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { usePageStore, type Page } from "@/store/usePageStore";
 import NavButton from "./NavButton";
+import { toast } from "react-toastify";
 
 const NavContainer = () => {
   const { setPage } = usePageStore();
@@ -13,8 +14,8 @@ const NavContainer = () => {
   return (
     <div className="w-72 phone:w-80 h-14 mx-auto relative rounded-[50px] bg-NavBg flex items-center justify-center">
       <NavButton text="Work" onClick={() => handleButtonClick('Work' as Page)} />
-      <NavButton text="Info" onClick={() => handleButtonClick('Info' as Page)} />
-      <NavButton text="Blog" onClick={() => handleButtonClick('Blog' as Page)} />
+      <NavButton text="Info" onClick={() => toast.info('Info Page Under Work')} />
+      <NavButton text="Blog" onClick={() => toast.info('Blog Page Under Work')} />
     </div>
   )
 };
